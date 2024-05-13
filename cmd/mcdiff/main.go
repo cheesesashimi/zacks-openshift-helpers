e9256c848645c0032d5378caefd486ba7e30636e
Zack Zlotnik <zzlotnik@redhat.com>
2024-05-13 16:02:00 -0400

    feat(mcdiff): initial commit of MachineConfig diffing tool

---
+++ cmd/mcdiff/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/spf13/cobra"
+	"k8s.io/component-base/cli"
+
+	versioncmd "github.com/cheesesashimi/zacks-openshift-helpers/internal/pkg/version"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "mcdiff",
+		Short: "Diffs MachineConfigs",
+		Long:  "",
+	}
+)
+
+var (
+	version = "not-built-properly"
+	commit  = "not-built-properly"
+	date    = "not-built-properly"
+)
+
+func init() {
+	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+	rootCmd.AddCommand(versioncmd.Command(version, commit, date))
+}
+
+func main() {
+	os.Exit(cli.Run(rootCmd))
+}
