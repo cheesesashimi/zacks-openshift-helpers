9577b96016c70089220515458b24600509df0cde
Zack Zlotnik <zzlotnik@redhat.com>
2026-01-20 10:21:02 -0500

    find merge commits

--- cmd/mcocheckout/internal/repo/repo.go
+++ cmd/mcocheckout/internal/repo/repo.go
@@ -1,20 +1,41 @@
 package repo
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"time"
 
 	"golang.org/x/mod/modfile"
 )
 
 const targetModule = "github.com/openshift/machine-config-operator"
 
+var pullRequestIDRegex = regexp.MustCompile(`\s#(\d+)\s`)
+
 type mcoRepo struct {
 	path string
+}
+
+type MergeCommit struct {
+	Timestamp time.Time
+	Commit    string
+	Subject   string
+}
+
+func (m *MergeCommit) GetPullRequestURL() (string, error) {
+	matches := pullRequestIDRegex.FindStringSubmatch(m.Subject)
+
+	if len(matches) > 1 {
+		return "https://github.com/openshift/machine-config-operator/pull/" + matches[1], nil
+	}
+
+	return "", fmt.Errorf("could not find a pull request ID in subject %q", m.Subject)
 }
 
 func NewMCORepo() (*mcoRepo, error) {
@@ -42,6 +63,43 @@ func (m *mcoRepo) IsClean() (bool, error) {
 
 	// Trim whitespace and check if the string length is zero
 	return len(strings.TrimSpace(out.String())) == 0, nil
+}
+
+func (m *mcoRepo) GetMergeCommits() ([]MergeCommit, error) {
+	cmd := exec.Command("git", "log", "--first-parent", "--format='%ai|%h|%s'")
+	cmd.Dir = m.path
+
+	buf := bytes.NewBuffer([]byte{})
+	cmd.Stdout = buf
+
+	err := cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get merge commits")
+	}
+
+	scanner := bufio.NewScanner(buf)
+
+	mc := []MergeCommit{}
+
+	layout := "2006-01-02 15:04:05 -0700"
+
+	for scanner.Scan() {
+		line := strings.ReplaceAll(scanner.Text(), "'", "")
+		splitLine := strings.Split(line, "|")
+
+		parsedTimestamp, err := time.Parse(layout, splitLine[0])
+		if err != nil {
+			return nil, fmt.Errorf("could not parse %q into time.Time: %w", splitLine[0], err)
+		}
+
+		mc = append(mc, MergeCommit{
+			Timestamp: parsedTimestamp,
+			Commit:    splitLine[1],
+			Subject:   splitLine[2],
+		})
+	}
+
+	return mc, nil
 }
 
 func (m *mcoRepo) Checkout(ref string) error {
